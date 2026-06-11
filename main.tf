a2ed42d05a99a82f3ee6c5cffda796b1fd5e0339
Janga Anjibabu <60913005+anjibabu006@users.noreply.github.com>
2026-06-11 11:14:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0152204c1a187337c"
   instance_type = "t3.micro"
   key_name      = "terraform"
-  count         = 4
+  count         = 2
   tags = {
     Name = "${var.ec2_name}-${count.index}"
   }
